34cf33e199edc18feb070a31b4bf8dfa1d01026c
mxd <838425805@qq.com>
2020-11-07 20:27:21 +0800

    去除不必要的提示

--- src/main/java/org/ssssssss/magicapi/config/MagicModule.java
+++ src/main/java/org/ssssssss/magicapi/config/MagicModule.java
@@ -1,5 +1,7 @@
 package org.ssssssss.magicapi.config;
 
+import org.ssssssss.script.annotation.UnableCall;
+
 /**
  * 模块，主要用于import指令，import时根据模块名获取当前类如：import assert;
  */
@@ -8,5 +10,6 @@ public interface MagicModule {
 	/**
 	 * 获取模块名
 	 */
-	public String getModuleName();
+	@UnableCall
+	String getModuleName();
 }
