19d30bc02d8d0c54cfbfa475d18e285ffb8c7bff
mxd <838425805@qq.com>
2021-03-27 20:48:03 +0800

    增加接口选项枚举PERMISSION、ROLE

--- src/main/java/org/ssssssss/magicapi/model/Options.java
+++ src/main/java/org/ssssssss/magicapi/model/Options.java
@@ -2,7 +2,9 @@ package org.ssssssss.magicapi.model;
 
 public enum Options {
 
-	WRAP_REQUEST_PARAMETERS("包装请求参数到一个变量中", "wrap_request_parameter");
+	WRAP_REQUEST_PARAMETERS("包装请求参数到一个变量中", "wrap_request_parameter"),
+	PERMISSION("允许拥有该权限的访问","permission"),
+	ROLE("允许拥有该角色的访问","role");
 
 	private final String name;
 	private final String value;
