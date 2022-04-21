7257870cb8217f4afca0da71547ac7ef5caae723
yuepeng <erupts@126.com>
2022-04-22 00:16:42 +0800

    增加Impala方言支持（大数据领域实时数据分析框架，比Hive效率强百倍）

---
+++ magic-api/src/main/java/org/ssssssss/magicapi/modules/db/dialect/ImpalaDialect.java
@@ -0,0 +1,22 @@
+package org.ssssssss.magicapi.modules.db.dialect;
+
+import org.ssssssss.magicapi.modules.db.BoundSql;
+
+/**
+ * @author YuePeng
+ * date 2022/4/22 00:10
+ */
+public class ImpalaDialect implements Dialect {
+
+    @Override
+    public boolean match(String jdbcUrl) {
+        return jdbcUrl.contains(":impala:");
+    }
+
+    @Override
+    public String getPageSql(String sql, BoundSql boundSql, long offset, long limit) {
+        boundSql.addParameter(offset);
+        boundSql.addParameter(limit);
+        return sql + " order by null limit ? offset ?";
+    }
+}
